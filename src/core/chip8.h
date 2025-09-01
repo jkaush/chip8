f3eb89484b543363ddf7ce1032996580b1ade6de
John Kaushagen <johnkaushagen@gmail.com>
2025-09-01 13:22:00 -0400

    Move contants to macro defines in chip8.h

--- src/core/chip8.h
+++ src/core/chip8.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#define MEMORY_SIZE 4096
+#define STACK_DEPTH 16
+#define V_REGISTER_COUNT 16
+
 #define CHIP8_WIDTH 64
 #define CHIP8_HEIGHT 32
 
@@ -10,11 +14,11 @@ extern const uint8_t chip8_fontset[80];
 extern const uint16_t font_offset;
 
 struct chip8_t {
-    uint8_t memory[4096];
-    uint8_t v_registers[16];
+    uint8_t memory[MEMORY_SIZE];
+    uint8_t v_registers[V_REGISTER_COUNT];
     uint16_t program_counter;
     uint16_t index_register;
-    uint16_t stack[16];
+    uint16_t stack[STACK_DEPTH];
     uint8_t stack_ptr;
     uint8_t delay_timer;
     uint8_t sound_timer;
